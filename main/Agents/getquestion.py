f424930df2196504659dc01b16992a09180551e7
chinmay-rr <f20220106@goa.bits-pilani.ac.in>
2024-11-29 17:12:44 +0530

    get q on elo

--- main/Agents/getquestion.py
+++ main/Agents/getquestion.py
@@ -1,5 +1,7 @@
 from main.DB.questions_query import getQuestion
 from main.DB.elo_question import get_latest_elo_entry
 
-def get_question_and_elo():
-    return getQuestion(get_latest_elo_entry())
+def get_question_and_elo(topic_weights: dict):
+    if not topic_weights:
+        return getQuestion(get_latest_elo_entry())
+    return getQuestion(topic_weights)
